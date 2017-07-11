83014f445bc93dc407559f6fd2dd70786a6869cf
LucasNass <lucasonass@gmail.com>
2017-07-10 23:25:43 -0300

    Aula 4

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule
