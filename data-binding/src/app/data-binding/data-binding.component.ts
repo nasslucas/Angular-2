da1d582f45b46a41cd6f8809a10958843a60f983
LucasNass <lucasonass@gmail.com>
2017-07-12 20:57:52 -0300

    Property binding + interpolation

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = 'http://loiane.training';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'http://lorempixel.com/400/200/technics/';
+
+  getValor()
+  {
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
   constructor() { }
 
   ngOnInit() {
