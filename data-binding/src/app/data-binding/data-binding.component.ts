09e278db1ba692b9b489caf7540c1bcb7f6ca67c
LucasNass <lucasonass@gmail.com>
2017-07-13 18:16:27 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,16 +10,39 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.training';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/technics/';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   getValor()
   {
     return 1;
   }
 
-  getCurtirCurso(){
+  getCurtirCurso()
+  {
     return true;
   }
 
+  botaoClicado()
+  {
+    alert('Botão clicado!');
+  }
+  
+  oneKeyUp(evento: KeyboardEvent)
+  {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+  
+  onMouseOverOut()
+  {
+    this.isMouseOver = !this.isMouseOver;
+  }
+  
   constructor() { }
 
   ngOnInit() {
